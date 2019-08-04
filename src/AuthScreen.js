0b1a8e9d98632079475d2c68bdc5b7890f138dfb
Gabriel Chaney <gabriel.chaney@gmail.com>
2019-08-03 20:54:17 -0700

    Add firebase client and login integration
    From https://github.com/venits/react-native-firebase-login-screen. Blows up with:
    ```
    [Unhandled promise rejection: FirebaseError: Firebase: No Firebase App '[DEFAULT]' has been created - call Firebase App.initializeApp() (app/no-app).]
    - node_modules/@firebase/util/dist/index.cjs.js:609:32 in FirebaseError
    - node_modules/@firebase/util/dist/index.cjs.js:642:38 in create
    - node_modules/@firebase/app/dist/index.rn.cjs.js:326:39 in app
    - node_modules/@firebase/app/dist/index.rn.cjs.js:388:49 in serviceNamespace
    * src/Firebase.js:6:6 in <unknown>
    - node_modules/promise/setimmediate/core.js:45:7 in tryCallTwo
    - node_modules/promise/setimmediate/core.js:200:23 in doResolve
    - node_modules/promise/setimmediate/core.js:66:12 in Promise
    * src/Firebase.js:5:23 in userLogin
    * src/AuthScreen.js:64:23 in loginToFireBase
    * src/AuthScreen.js:55:29 in <unknown>
    - node_modules/react-native/Libraries/Renderer/oss/ReactNativeRenderer-dev.js:13857:16 in callCallback
    - node_modules/react-native/Libraries/Renderer/oss/ReactNativeRenderer-dev.js:13901:19 in commitUpdateEffects
    - ... 21 more stack frames from framework internals
    ```

--- src/AuthScreen.js
+++ src/AuthScreen.js
@@ -5,12 +5,14 @@ import {
   View,
 } from 'react-native';
 import {
+  ActivityIndicator,
   Button,
   Text,
   TextInput,
   Title,
   withTheme } from 'react-native-paper';
 import type { Theme } from 'react-native-paper/types';
+import Firebase from 'src/Firebase';
 
 type Props = {
   theme: Theme,
@@ -29,11 +31,61 @@ class AuthScreen extends React.Component<Props, State> {
   state = {
     email: '',
     password: '',
+    isEmailCorrect: false,
+    isPasswordCorrect: false,
+    isLogin: false,
+    loading: false,
   };
 
   constructor(props) {
     super(props);
     this.passTextInput = null;
+  }
+
+  onButtonPress() {
+    this.setState({ error: '', loading: true })
+    const email = this.state.email;
+    const password = this.state.password;
+
+    this.setState({
+      isEmailCorrect: email === '',
+      isPasswordCorrect: password === '',
+    }, () => {
+      if(email !== '' && password !== ''){
+        this.loginToFireBase(email, password);
+      } else {
+        console.warn('Please enter your email and password')
+      }
+    });
+  };
+
+  loginToFireBase(email, password) {
+    this.setState({ isLogin: true });
+    Firebase.userLogin(email, password)
+      .then(user => {
+        if(user) this.props.success(user);
+        this.setState({ isLogin: false });
+      });
+  };
+
+  renderButton() {
+    if (this.state.loading) {
+      return (
+        <View style={styles.spinnerStyle}>
+          <ActivityIndicator size={"small"} />
+        </View>
+      )
+    } else {
+      return (
+        <Button
+          mode="outlined"
+          onPress={this.onButtonPress.bind(this)}
+          style={styles.button}
+        >
+          Log in
+        </Button>
+      )
+    }
   }
 
   render() {
@@ -53,35 +105,35 @@ class AuthScreen extends React.Component<Props, State> {
 
         <TextInput
           autoCompleteType="email"
-          mode="outlined"
-          style={styles.inputContainerStyle}
-          label="Email"
           keyboardType="email-address"
+          label="Email"
+          mode="outlined"
           placeholder="example@gmail.com"
-          value={this.state.email}
           onChangeText={email => this.setState({ email })}
-          onSubmitEditing={() => { this.passTextInput.focus(); }}
+          onSubmitEditing={() => {this.passTextInput.focus()}}
           returnKeyType='next'
+          style={styles.inputContainerStyle}
+          value={this.state.email}
         />
 
         <TextInput
           autoCompleteType="password"
-          ref={(input) => {
-              this.passTextInput = input
-          }}
-          mode="outlined"
-          style={styles.inputContainerStyle}
           label="Password"
+          mode="outlined"
           placeholder="$tr0ngPassword"
-          value={this.state.password}
           onChangeText={password => this.setState({ password })}
-          returnKeyType="next"
           secureTextEntry={true}
+          ref={input => {this.passwordTextInput = input}}
+          returnKeyType="next"
+          style={styles.inputContainerStyle}
+          value={this.state.password}
         />
 
-        <Button mode="outlined" onPress={()=>this.props.navigation.navigate('home')} style={styles.button}>
-          Login
-        </Button>
+        {this.renderButton()}
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
       </View>
     );
   }
@@ -93,17 +145,27 @@ const styles = StyleSheet.create({
     marginTop: 25,
     padding: 8,
   },
+  errorTextStyle: {
+    fontSize: 16,
+    alignSelf: 'center',
+    color: 'red'
+  },
   textInput: {
     margin: 8,
   },
   button: {
     height: 55,
-    marginTop: 15,
+    marginTop: 8,
     justifyContent: 'center',
   },
   title: {
     margin: 8,
   },
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default withTheme(AuthScreen);
